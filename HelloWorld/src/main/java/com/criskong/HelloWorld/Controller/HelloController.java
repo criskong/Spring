3ddd8d886346e8e4b11147a942b78da3123f676a
criskong <antonio.criscuolo001@studenti.uniparthenope.it>
2026-02-28 17:06:16 +0100

    Primo commit OrderApp

--- HelloWorld/src/main/java/com/criskong/HelloWorld/Controller/HelloController.java
+++ HelloWorld/src/main/java/com/criskong/HelloWorld/Controller/HelloController.java
@@ -1,12 +1,15 @@
-package com.criskong.HelloWorld;
+package com.criskong.HelloWorld.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+//serve per ricevere domande dall'url e restituire risposte.
 @RestController
 public class HelloController {
 
+    //Viene utilizzato nel momento in cui nell'url dopo il percorso principale,
+    //viene inserito /hello e mostra a video la stringa associata a quell'url
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
--- HelloWorld/src/main/java/com/criskong/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/criskong/HelloWorld/HelloWorldApplication.java
@@ -1,8 +1,10 @@
+//classe principale, punto di ingresso dell'applicazione.
 package com.criskong.HelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//configurazione iniziale springBoot
 @SpringBootApplication
 public class HelloWorldApplication {
 
